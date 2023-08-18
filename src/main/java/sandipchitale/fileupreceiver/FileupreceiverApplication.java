832001f7e8b7bf20d91ae216cd70bcaed91d2f5c
Sandip Chitale <schitale@opentext.com>
2023-08-18 16:54:51 -0700

    Fixes.

--- src/main/java/sandipchitale/fileupreceiver/FileupreceiverApplication.java
+++ src/main/java/sandipchitale/fileupreceiver/FileupreceiverApplication.java
@@ -4,14 +4,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.fileupload2.core.FileItemInput;
 import org.apache.commons.fileupload2.core.FileItemInputIterator;
 import org.apache.commons.fileupload2.jakarta.JakartaServletFileUpload;
+import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
+import java.io.*;
 
 @SpringBootApplication
 public class FileupreceiverApplication {
@@ -28,21 +30,28 @@ public class FileupreceiverApplication {
 		}
 
 		@PostMapping("/receive")
-		public String receive(HttpServletRequest httpServletRequest) {
+		public ResponseEntity<String> receive(HttpServletRequest httpServletRequest) {
 			boolean multipartContent = JakartaServletFileUpload.isMultipartContent(httpServletRequest);
 			if (multipartContent) {
 				JakartaServletFileUpload jakartaServletFileUpload = new JakartaServletFileUpload();
 				try {
 					FileItemInputIterator itemIterator = jakartaServletFileUpload.getItemIterator(httpServletRequest);
-					if (itemIterator.hasNext()) {
+					while (itemIterator.hasNext()) {
 						FileItemInput fileItemInput = itemIterator.next();
-						return "Received file: " + fileItemInput.getFieldName() + " content type: " + fileItemInput.getContentType();
+						System.out.println("Received file: Field: " + fileItemInput.getFieldName() + " Filename: " + fileItemInput.getName() + " content type: " + fileItemInput.getContentType());
+
+
+						File receivedFile = new File(fileItemInput.getName());
+						try (FileOutputStream fos = new FileOutputStream(receivedFile)) {
+							IOUtils.copy(fileItemInput.getInputStream(), fos, 4096);
+							System.out.println("Wrote received file to: " + receivedFile.getAbsolutePath());
+						}
 					}
 				} catch (IOException e) {
 					throw new RuntimeException(e);
 				}
 			}
-			return "File Receiver Application! Send file!";
+			return ResponseEntity.ok("File Receiver Application! Received files");
 		}
 	}
 
